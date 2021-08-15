441a679d812e0c1277a91d8db9c314d84723e4d6
JoCarrasco <jogregcarrasco@gmail.com>
2021-08-15 16:08:41 -0400

    Added constants and Mikro ORM setup.
    Removed console log.

---
+++ src/entities/TimeEntry.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+@Entity()
+export class TimeEntry {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  startedAt = new Date();
+
+  @Property()
+  endedAt = new Date();
+
+  @Property()
+  title!: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('HEllo WORLD');
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init(
+    {
+      dbName: 'timetracker',
+      type: 'postgresql',
+      debug: !__prod__
+    }
+  );
+};
